124ecf9acfa176161f1ae2bc00c10568c635cde2
Stefano Braghin <stefano.braghin@gmail.com>
2013-06-14 17:43:35 +0100

    stub

--- src/main/java/org/fortytwo/developers/mybudget0123/client/MyBudget0123.java
+++ src/main/java/org/fortytwo/developers/mybudget0123/client/MyBudget0123.java
@@ -1,13 +1,14 @@
 package org.fortytwo.developers.mybudget0123.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 
 public class MyBudget0123 implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		// TODO Auto-generated method stub
-
+		RootLayoutPanel.get().add(new Label("THIS IS A TEST!!"));
 	}
 
 }
