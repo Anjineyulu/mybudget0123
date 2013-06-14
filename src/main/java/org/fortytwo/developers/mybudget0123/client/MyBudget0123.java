b838aec0df29513f5180964b20fd7f2dbb23dc9a
Stefano Braghin <stefano.braghin@gmail.com>
2013-06-14 17:25:47 +0100

    initial

---
+++ src/main/java/org/fortytwo/developers/mybudget0123/client/MyBudget0123.java
@@ -0,0 +1,13 @@
+package org.fortytwo.developers.mybudget0123.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class MyBudget0123 implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
